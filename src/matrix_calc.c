ba751e331ea3f6265757a2dc341bca14eddfc61e
Frank Higley <fahigley@gamil.com>
2023-10-22 14:47:57 -0400

    Added documentation

--- src/matrix_calc.c
+++ src/matrix_calc.c
@@ -4,6 +4,9 @@
 
 //enum and structs
 
+/**
+ * enums for user interface
+ */
 enum Option{
 	QUIT = 'q',
 	HELP = 'h',
@@ -12,6 +15,13 @@ enum Option{
 	DETERMINANT = 'd',
 };
 
+/**
+ * struct for matrix
+ *
+ * @param rows number of rows
+ * @param cols number of columns
+ * @param data 2d array that stores matrix values
+ */
 typedef struct{
 	int rows; //number of rows
 	int cols; // number of columns
@@ -21,11 +31,18 @@ typedef struct{
 
 //functions
 
+/**
+ * help menu printout
+ */
 void helpMenu(){
 	printf("HELP MENU\n");
 }
 
-
+/**
+ * initializes a matrix struct
+ *
+ * @return matrix struct
+ */
 Matrix initializeMatrix(){
 	//initialize matrix
 	Matrix matrix;
@@ -63,7 +80,11 @@ Matrix initializeMatrix(){
     return matrix;
 }
 
-
+/**
+ * frees the memory of the given matrix
+ *
+ * @param mat given matrix to be freed
+ */
 void freeMatrix(Matrix *mat){\
 	//iterate throught the matrix and free memory
 	for(int i = 0; i < mat->rows; i++){
@@ -73,7 +94,11 @@ void freeMatrix(Matrix *mat){\
 	free(mat);
 }
 
-
+/**
+ * prints a given matrix to the terminal
+ *
+ * @param mat given matrix to be printed
+ */
 void printMatrix(Matrix *mat){
 	//iterate through the matrix and print each element
 	if(mat->data == NULL){
@@ -92,7 +117,13 @@ void printMatrix(Matrix *mat){
 }
 
 
-
+/**
+ * swaps two given rows in a matrix
+ *
+ * @param mat given matrix
+ * @param row1 first row to be swapped
+ * @param row2 second row to be swapped
+ */
 void swapRows(Matrix *mat, int row1, int row2){
 	//check for valid row1 and row 2 values
 	if (row1 < 0 || row1 >= mat->rows || row2 < 0 || row2 >= mat->rows) {
@@ -125,7 +156,16 @@ void swapRows(Matrix *mat, int row1, int row2){
 }
 
 
-//adds row 2 to row 1 by the multiple factor
+/**
+ * adds  a multiple of row2 to row 1
+ * set factor to 1 to keep same values
+ *
+ * @param mat given matrix
+ * @param row1 first row
+ * @param row2 second row
+ * @param factor multiple row 2 will be multiplied
+ *
+ */
 void addRowsByMultiple(Matrix *mat, int row1, int row2, float factor){
 	if(mat == NULL){
 		return;
@@ -138,6 +178,13 @@ void addRowsByMultiple(Matrix *mat, int row1, int row2, float factor){
 }
 
 
+/**
+ * multiplies a given row by a multiplier
+ *
+ * @param mat given matrix
+ * @param row given row to be multiplied
+ * @param mulitplier given multiplier
+ */
 void multiplyRowBy(Matrix *mat, int row, int multiplier){
 	if(mat == NULL){
 		return;
@@ -149,7 +196,12 @@ void multiplyRowBy(Matrix *mat, int row, int multiplier){
 	
 }
 
-void ReducedRowEchelonForm(Matrix *mat){
+/**
+ * Reduces a given matrix to Reduced row echelon form
+ *
+ * @param mat given matrix
+ */
+void reducedRowEchelonForm(Matrix *mat){
 	int lead = 0;
 	int rowCount = mat->rows;
 	int columnCount = mat->cols;
@@ -182,23 +234,36 @@ void ReducedRowEchelonForm(Matrix *mat){
 	}
 }
 
-
+/**
+ * driver function for the reduced row echelon calculation
+ */
 void rowEchelonFormDriver(){
 	Matrix mat = initializeMatrix();
 	Matrix *pmat = &mat;
 	printMatrix(pmat);
 	printf("The Reduced Row Echelon Form is:\n");
-	ReducedRowEchelonForm(pmat);
+	reducedRowEchelonForm(pmat);
 	printMatrix(pmat);
 
 	freeMatrix(pmat);
 	pmat = NULL;
 }
 
+/**
+ * calculates the determinant for a 3x3 matrix
+ *
+ * @param a (1,1) of the given matrix
+ * @param b (1,2) of the given matrix
+ * @param c (2,1) of the given matrix
+ * @param d (2,2) of the given matrix
+ */
 int determinant2x2(int a, int b, int c, int d){
   return (a*d)-(b*c);
 }
 
+/**
+ * driver for the determinant calculation
+ */
 void determinantDriver(){
   printf("As of now the determinant calculation only supports 2x2 and 3x3 matricies:\n");
   Matrix mat = initializeMatrix();
@@ -226,13 +291,17 @@ void determinantDriver(){
 }
 
 
-
+/**
+ * extra function for testing
+ */
 void test(){
 }
 
 
-//Main method
 
+/**
+ * main driver function
+ */
 int main(){
  	printf("Welcome to matrix Calculator!\n");
  	printf("This apps purpose is to help with matrix calculations\n");
